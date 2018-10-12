6791d8df738a3bec576568b76c3033c1f2321152
Stefan Behl <stefan.behl@bosch-si.com>
2018-10-12 15:33:50 +0200

    Add slides for part 1 and part 2
    Signed-off-by: Stefan Behl <stefan.behl@bosch-si.com>

--- docs/js/reveal.js
+++ docs/js/reveal.js
@@ -39,7 +39,7 @@
 
 			// The "normal" size of the presentation, aspect ratio will be preserved
 			// when the presentation is scaled to fit different resolutions
-			width: 960,
+			width: 1024,
 			height: 700,
 
 			// Factor of the display size that should remain empty around the content
